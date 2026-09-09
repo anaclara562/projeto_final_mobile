fbe67e20597f086686b78fe19f62c8fa0c228465
anaclara562 <anaclaraa.rois2@gmail.com>
2026-09-09 10:03:54 -0300

    segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'lêBrasil',
       theme: ThemeData(
         colorSchemeSeed: const Color(0xFFDFF6FF),
@@ -73,6 +74,32 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 }
 
+class AppInfo {
+  final String nome;
+  final String descricao;
+  final IconData icone;
+
+  const AppInfo({
+    required this.nome,
+    required this.descricao,
+    required this.icone,
+  });
+}
+
+final List<AppInfo> meusApps=[
+  AppInfo(
+    nome: 'Calculadora de Gasolina',
+    descricao: 'Calcula litros e custo de uma viagem',
+    icone: Icons.local_gas_station,
+  ),
+  AppInfo(
+    nome: 'Calculadora de Churrasco',
+    descricao: 'Calcula carne, bebida e carvão',
+    icone: Icons.outdoor_grill,
+  ),
+];
+
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
@@ -91,6 +118,40 @@ class HomeScreen extends StatelessWidget {
         elevation: 0,
         centerTitle: true,
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration:BoxDecoration(
+                color: Theme.of(context).colorScheme.primary, 
+              ),
+              child: const Align(
+                alignment: AlignmentGeometry.bottomLeft,
+                child: Text(
+                  'Central de Apps',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Início'),
+              onTap: () => Navigator.pop(context),
+            ), 
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Perfil'),
+              onTap: () => Navigator.pop(context),
+            ), 
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Sair'),
+              onTap: () => Navigator.pop(context),
+            ), 
+          ]
+        ),
+      ),
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
@@ -102,21 +163,51 @@ class HomeScreen extends StatelessWidget {
             ],
           ),
         ),
-        child: Center(
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            decoration: BoxDecoration(
-              color: const Color(0xFFBEEBFA),
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: const Text(
-              'Em construção...',
-              style: TextStyle(
-                color: Color(0xFF3B98B7),
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+        child: GridView.builder(
+          padding: EdgeInsets.all(16),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 12,
+            crossAxisSpacing: 12,
+            childAspectRatio: 0.95,
           ),
+          itemCount: meusApps.length,
+          itemBuilder: (context, indice){
+            final app = meusApps[indice];
+            return Card(
+              child: Padding(
+                padding: EdgeInsets.all(12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      app.icone,
+                      size: 36,
+                      color: Theme.of(context).colorScheme.primary,
+                    
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      app.nome,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      app.descricao,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 11,
+                      ),
+                    ),
+                  ],
+                ), 
+              )
+            );
+          },
         ),
       ),
     );
